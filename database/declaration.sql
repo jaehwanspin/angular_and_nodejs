9a3d2eb381f934c4d107cb7cb1343d9b4440e3ce
jaehwanspin <jaehwanspin@gmail.com>
2019-02-27 18:21:24 +0900

    ddddbbbbbb

--- database/declaration.sql
+++ database/declaration.sql
@@ -1,13 +1,21 @@
+DROP DATABASE IF EXISTS jaehwanspin;
+CREATE DATABASE jaehwanspin CHARACTER SET 'utf8' COLLATE 'utf8_bin';
 USE jaehwanspin;
 
+DROP VIEW IF EXISTS vw_normaluser;
+DROP VIEW IF EXISTS vw_normalboard;
+DROP VIEW IF EXISTS vw_normalcomment;
+DROP VIEW IF EXISTS vw_normalfile;
 
-DROP TABLE if EXISTS tbl_boardFile CASCADE;
-DROP TABLE if EXISTS tbl_file CASCADE;
-DROP TABLE if EXISTS tbl_comment CASCADE;
+DROP TABLE IF EXISTS tbl_boardFile CASCADE;
+DROP TABLE IF EXISTS tbl_file CASCADE;
+DROP TABLE IF EXISTS tbl_comment CASCADE;
 DROP TABLE IF EXISTS tbl_board CASCADE;
+DROP TABLE if EXISTS tbl_category CASCADE;
 DROP TABLE IF EXISTS tbl_user CASCADE;
 
 
+# 사용자
 CREATE TABLE tbl_user (
 
     usNo 			INT 			  	NOT NULL	AUTO_INCREMENT		
@@ -20,14 +28,31 @@ CREATE TABLE tbl_user (
   , remDate 		DATETIME 		NULL
   
   , CONSTRAINT pk_user_usNo PRIMARY KEY(usNo)
+  , CONSTRAINT uq_user_usId UNIQUE(usId)
+  , CONSTRAINT uq_user_usEmail UNIQUE(usEmail)
   
 )
 ;
 
 
+CREATE TABLE tbl_category (
+
+    catNo			INT				NOT NULL AUTO_INCREMENT
+  , catName			VARCHAR(255)	NOT NULL
+  , catAvailable	BOOL				NOT NULL DEFAULT 1
+  
+  , CONSTRAINT pk_category_catNo PRIMARY KEY(catNo)
+  , CONSTRAINT uq_category_catName UNIQUE(catName)
+
+)
+;
+
+
+# 게시판
 CREATE TABLE tbl_board (
 	
 	 boNo 			INT 				NOT NULL AUTO_INCREMENT
+  , catNo			INT				NOT NULL
   , boTitle 		VARCHAR(255) 	NOT NULL
   , boContent 		TEXT 				NOT NULL
   , readCount 		INT 				NOT NULL DEFAULT 0
@@ -40,11 +65,14 @@ CREATE TABLE tbl_board (
   , CONSTRAINT pk_board_boNo PRIMARY KEY(boNo)
   , CONSTRAINT fk_board_writer FOREIGN KEY(writer)
   	 	REFERENCES tbl_user(usNo) ON DELETE CASCADE
+  , CONSTRAINT fk_board_catNo FOREIGN KEY(catNo)
+  	 	REFERENCES tbl_category(catNo) ON DELETE CASCADE
   	 	
 )
 ;
 
 
+# 댓글
 CREATE TABLE tbl_comment (
 
     comNo			INT				NOT NULL AUTO_INCREMENT
@@ -61,6 +89,7 @@ CREATE TABLE tbl_comment (
 );
 
 
+# 파일
 CREATE TABLE tbl_file (
 
 	 fileNo 			INT 				NOT NULL AUTO_INCREMENT
@@ -68,13 +97,14 @@ CREATE TABLE tbl_file (
   , fileDir 		VARCHAR(255) 	NOT NULL
   , fileExt 		VARCHAR(255) 	NULL
   , fileAvailable BOOL 				NOT NULL DEFAULT 1
-#  , 
+ 
   , CONSTRAINT pk_file_fileNo PRIMARY KEY(fileNo)
   
 )
 ;
 
 
+# 게시판 파일
 CREATE TABLE tbl_boardFile (
 	 
 	 fileNo 			INT 				NOT NULL
@@ -87,6 +117,59 @@ CREATE TABLE tbl_boardFile (
   	 	REFERENCES tbl_board(boNo) ON DELETE CASCADE
 
 );
+
+
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW vw_normalUser
+	AS SELECT usNo
+	        , usId
+	        , usEmail
+	        , usName
+	        , regDate
+	        , modDate
+	     FROM tbl_user
+	    WHERE remDate IS NULL
+;
+
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW vw_normalboard
+	AS SELECT boNo
+	        , boTitle
+	        , boContent
+	        , readCount
+	        , recomCount
+	        , writer
+	        , regDate
+	        , modDate
+	     FROM tbl_board
+	    WHERE remDate IS NULL
+;
+
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW vw_normalcomment
+	AS SELECT comNo
+	        , comContent
+	        , writer
+	        , regDate
+	        , modDate
+		  FROM tbl_comment
+		 WHERE remDate IS NULL
+;
+
+
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW vw_normalfile
+	AS SELECT fileNo
+	        , fileName
+	        , fileDir
+	        , fileExt
+  	     FROM tbl_file
+  	    WHERE fileAvailable = 1
+;
+
+
+
+
+
 
 
 COMMIT;
